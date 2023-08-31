aa2bc215753b540950d312c8a813a0b5e9f1eeb3
Chachou <chachou@mbp-de-chachou.lan>
2023-08-31 12:23:50 +0200

    update

--- routes/comics.js
+++ routes/comics.js
@@ -3,6 +3,9 @@ const router = express.Router();
 require("dotenv").config();
 const axios = require("axios");
 
+const app = express();
+app.use(express.json());
+
 router.get("/comics", async (req, res) => {
   const my_api_key = process.env.MY_API_KEY;
   let filters = {};
@@ -13,9 +16,9 @@ router.get("/comics", async (req, res) => {
     const limit = req.query.limit || 100;
 
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${my_api_key}&title=${title}&skip=${skip}&limi=${limit}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${my_api_key}&title=${title}&skip=${skip}&limit=${limit}`
     );
-
+    console.log(response.data);
     res.status(200).json(response.data);
   } catch (error) {
     console.log(error.message);
